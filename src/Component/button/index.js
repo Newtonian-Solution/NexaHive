ff062632ad1801e7f507e95bebcee2c95ac4afd2
Jubril <18-30gd050@students.unilorin.edu.ng>
2022-11-09 11:46:09 +0100

    make it  better

--- src/Component/button/index.js
+++ src/Component/button/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Button.scss";
+import "./index.scss";
 
 function Button({ onclick, className, children }) {
   return <button className={`btn ${className}`}>{children}</button>;
--- src/Component/nav/index.js
+++ src/Component/nav/index.js
@@ -1,47 +1,47 @@
-import React,{useState} from "react";
-import Logo from '../../Assets/Images/Logo.png'
-import './index.scss'
-import {Link} from 'react-router-dom'
-import Button from '../button/Button'
+import React, { useState } from "react";
+import Logo from "../../Assets/Images/Logo.png";
+import "./index.scss";
+import { Link } from "react-router-dom";
+import Button from "../button";
 const Nav = () => {
-    const [checked, setChecked] = useState(false)
-    return (
-      <nav className="nav">
-        <div className="nav__logo--container">
-          <img src={Logo} alt="nexahive-logo" />
-        </div>
-        <ul className={`nav__items ${checked && 'active'}`}>
-          <li className="nav__items--item">
-            <Link>Home</Link>
-          </li>
-          <li className="nav__items--item">
-            <Link>About Us</Link>
-          </li>
-          <li className="nav__items--item">
-            <Link>Services</Link>
-          </li>
-          <li className="nav__items--item">
-            <Link>Projects</Link>
-          </li>
-          <li className="nav__items--item">
-            <Link>Team</Link>
-          </li>
-        </ul>
-        <Button className={"nav__btn"}>Contact Us</Button>
-        <div className="nav__icon">
-          <input
-            type="checkbox"
-            className={`navigation__checkbox`}
-            id="navi-toggle"
-            checked={checked}
-            onChange={(e) => setChecked(e.target.checked)}
-          />
-          <label htmlFor="navi-toggle" className={`navigation__button`}>
-            <span className={`navigation__icon`}>&nbsp;</span>
-          </label>
-        </div>
-      </nav>
-    );
+  const [checked, setChecked] = useState(false);
+  return (
+    <nav className="nav">
+      <div className="nav__logo--container">
+        <img src={Logo} alt="nexahive-logo" />
+      </div>
+      <ul className={`nav__items ${checked && "active"}`}>
+        <li className="nav__items--item">
+          <Link>Home</Link>
+        </li>
+        <li className="nav__items--item">
+          <Link>About Us</Link>
+        </li>
+        <li className="nav__items--item">
+          <Link>Services</Link>
+        </li>
+        <li className="nav__items--item">
+          <Link>Projects</Link>
+        </li>
+        <li className="nav__items--item">
+          <Link>Team</Link>
+        </li>
+      </ul>
+      <Button className={"nav__btn"}>Contact Us</Button>
+      <div className="nav__icon">
+        <input
+          type="checkbox"
+          className={`navigation__checkbox`}
+          id="navi-toggle"
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
+        <label htmlFor="navi-toggle" className={`navigation__button`}>
+          <span className={`navigation__icon`}>&nbsp;</span>
+        </label>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
